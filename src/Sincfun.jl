928804a427b6f93080497b2d03715771166c6981
Richard Mikael Slevinsky <rms8@ualberta.ca>
2014-09-18 12:24:29 -0600

    updates

--- src/Sincfun.jl
+++ src/Sincfun.jl
@@ -10,12 +10,12 @@ type Domain
 	psip::Function
 end
 
-#For functions on a Finite domain with algebraic and logarithmic endpoint singularities, use the numbers α, β, γ, and δ to compute the singularities in a stable way.
-Finite{T<:Number}(α::T,β::T,γ::T,δ::T) = Domain(t->tanh(t),t->atanh(t),t->sech(t).^2.*(2./(exp(2t).+1)).^α.*(2./(exp(-2t).+1)).^β.*log(2./(exp(2t).+1)).^γ.*log(2./(exp(-2t).+1)).^δ)
+Finite = Domain(t->tanh(t),t->atanh(t),t->sech(t).^2)
 Infinite = Domain(t->sinh(t),t->asinh(t),t->cosh(t))
 SemiInfinite1 = Domain(t->log(exp(t)+1),t->log(exp(t)-1),t->1./(1+exp(-t)))
 SemiInfinite2 = Domain(t->exp(t),t->log(t),t->exp(t))
 
+#For functions on a Finite domain with algebraic and logarithmic endpoint singularities, use the numbers α, β, γ, and δ to compute the singularities in a stable way.
 type sincfun{F<:Function,D<:Domain,T<:Number}
 	n::Integer
 	h::T
@@ -24,8 +24,12 @@ type sincfun{F<:Function,D<:Domain,T<:Number}
 	ωv::Vector{T}
 	j::Vector{Int64}
 	domain::D
+	α::T
+	β::T
+	γ::T
+	δ::T
 	ω::F
-	function sincfun{F<:Function,D<:Domain,T<:Number}(f::F,domain::D,::Type{T})
+	function sincfun{F<:Function,D<:Domain,T<:Number}(f::F,domain::D,::Type{T},α::T,β::T,γ::T,δ::T)
 		Tone,Tpi = one(T),convert(T,pi)
 		ω(t) = exp(-Tpi/2*cosh(t))
 		n=2^3
@@ -33,7 +37,7 @@ type sincfun{F<:Function,D<:Domain,T<:Number}
 		jh = h*[-n:n]
 		sinhv,coshv = Tpi/2*sinh(jh),Tpi/2*cosh(jh)
 		fphiv = f(domain.psi(sinhv))
-		phipv = domain.psip(sinhv).*coshv
+		phipv = domain.psip(sinhv).*coshv.*(2./(exp(2sinhv).+1)).^α.*(2./(exp(-2sinhv).+1)).^β.*log(2./(exp(2sinhv).+1)).^γ.*log(2./(exp(-2sinhv).+1)).^δ
 		ωv = ω(jh)
 		test = abs(fphiv.*phipv)
 		cutoff = !isinf(test).*!isnan(test)
@@ -47,7 +51,7 @@ type sincfun{F<:Function,D<:Domain,T<:Number}
 			jh = h*[-n:n]
 			sinhv,coshv = Tpi/2*sinh(jh),Tpi/2*cosh(jh)
 			fphiv = f(domain.psi(sinhv))
-			phipv = domain.psip(sinhv).*coshv
+			phipv = domain.psip(sinhv).*coshv.*(2./(exp(2sinhv).+1)).^α.*(2./(exp(-2sinhv).+1)).^β.*log(2./(exp(2sinhv).+1)).^γ.*log(2./(exp(-2sinhv).+1)).^δ
 			ωv = ω(jh)
 			test = abs(fphiv.*phipv)
 			cutoff = !isinf(test).*!isnan(test)
@@ -57,23 +61,23 @@ type sincfun{F<:Function,D<:Domain,T<:Number}
 			intold,intnew = intnew,h*dot(fphiv,phipv)
 			println(intold," ",intnew," ",n)
 		end
-		j = [-n:n]
-		test = abs(fphiv.*phipv)
-		cutoff = !isinf(test).*!isnan(test)
+		j=[-n:n]
 		n=length(cutoff[cutoff.==true])
 		println(length(cutoff)," ",length(fphiv))
-		fphiv = fphiv[cutoff]
-		phipv = phipv[cutoff]
-		ωv = ωv[cutoff]
 		j = j[cutoff]
 		println(intnew," ",h*dot(fphiv,phipv)," ",n)
-		new(n,h,fphiv,phipv,ωv,j,domain,ω)
+		new(n,h,fphiv,phipv,ωv,j,domain,α,β,γ,δ,ω)
 	end
 end
-sincfun{F<:Function,D<:Domain,T<:Number}(f::F,domain::D,::Type{T}) = sincfun{F,D,T}(f,domain,T)
-sincfun{F<:Function,D<:Domain}(f::F,domain::D) = sincfun{F,D,Float64}(f,domain,Float64)
-sincfun{F<:Function,T<:Number}(f::F,::Type{T}) = sincfun{F,Domain,T}(f,Finite(0,0,0,0),T)
-sincfun{F<:Function}(f::F) = sincfun{F,Domain,Float64}(f,Finite(0,0,0,0),Float64)
+#General Domain constructors
+sincfun{F<:Function,D<:Domain,T<:Number}(f::F,domain::D,::Type{T}) = sincfun{F,D,T}(f,domain,T,zero(T),zero(T),zero(T),zero(T))
+sincfun{F<:Function,D<:Domain}(f::F,domain::D) = sincfun{F,D,Float64}(f,domain,Float64,0.0,0.0,0.0,0.0)
+
+#Finite Domain constructors
+sincfun{F<:Function,T<:Number}(f::F,::Type{T},α::T,β::T,γ::T,δ::T) = sincfun{F,Domain,T}(f,Finite,T,α,β,γ,δ)
+sincfun{F<:Function,T<:Number}(f::F,::Type{T}) = sincfun{F,Domain,T}(f,Finite,T,zero(T),zero(T),zero(T),zero(T))
+sincfun{F<:Function}(f::F,α::Float64,β::Float64,γ::Float64,δ::Float64) = sincfun{F,Domain,Float64}(f,Finite,Float64,α,β,γ,δ)
+sincfun{F<:Function}(f::F) = sincfun{F,Domain,Float64}(f,Finite,Float64,0.0,0.0,0.0,0.0)
 
 function barycentric{F<:Function,D<:Domain,T<:Number}(sf::sincfun{F,D,T},x::T)
 	Tpi = convert(T,pi)
@@ -95,7 +99,6 @@ function barycentric{F<:Function,D<:Domain,T<:Number}(sf::sincfun{F,D,T},x::T)
 				valD += sf.ωv[j]/common
 			end
 			=#
-			
 			for j=1:2:sf.n
 				common = t-sf.j[j]*sf.h
 				valN += sf.fphiv[j]*sf.phipv[j]/common
@@ -106,7 +109,6 @@ function barycentric{F<:Function,D<:Domain,T<:Number}(sf::sincfun{F,D,T},x::T)
 				valN -= sf.fphiv[j]*sf.phipv[j]/common
 				valD -= sf.ωv[j]/common
 			end
-			
 			return sf.ω(t)/sf.domain.psip(Tpi/2*sinh(t))/(Tpi/2)/cosh(t)*valN/valD
 		else
 			return sf.ω(t)/sf.domain.psip(Tpi/2*sinh(t))/(Tpi/2)/cosh(t)*sf.fphiv[idx]*sf.phipv[idx]/sf.ωv[idx]
@@ -122,7 +124,8 @@ function Base.getindex{F<:Function,D<:Domain,T<:Number}(sf::sincfun{F,D,T},x::Ve
 	end
 	return sfx
 end
-
+Base.length(sf::sincfun) = sf.n
+Base.sum(sf::sincfun) = sf.h*dot(sf.fphiv,sf.phipv)
 
 include("fftBigFloat.jl")
 include("KrylovMethods.jl")
