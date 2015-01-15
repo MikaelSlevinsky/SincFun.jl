8557e5fd8b5181529f9fc3ff3b6839c478d8accb
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2015-01-15 08:59:11 +0000

    Added singularities function
    Also base constructor on 2-norm convergence

--- src/Domains.jl
+++ src/Domains.jl
@@ -4,7 +4,7 @@ export Domain,Finite,Infinite1,Infinite2,SemiInfinite1,SemiInfinite2
 
 abstract Domain{T}
 
-#For functions on a Finite domain with algebraic and logarithmic endpoint singularities, use the numbers α, β, γ, and δ to compute the singularities in a stable way.
+# For functions on a Finite domain with algebraic and logarithmic endpoint singularities, use the numbers α, β, γ, and δ to compute the singularities in a stable way.
 
 type Finite{T} <: Domain{T}
     ψ::Function
@@ -14,10 +14,11 @@ type Finite{T} <: Domain{T}
     algebraic::Vector{T} # exponents of the algebraic singularities of the endpoints
     logarithmic::Vector{T} # exponents of the logarithmic singularities of the endpoints
 end
-Finite{T<:Number}(a::T,b::T,α::T,β::T,γ::T,δ::T) = Finite{T}(t->(a+b)/2+(b-a)/2*tanh(t),t->atanh((2t-a-b)/(b-a)),t->(b-a)/2*sech(t).^2,[a,b],[α,β],[γ,δ]) #Algebraic and logarithmic endpoint singularities
-Finite{T<:Number}(a::T,b::T,α::T,β::T) = Finite(a,b,α,β,zero(T),zero(T)) #Only algebraic endpoint singularities
-Finite{T<:Number}(a::T,b::T) = Finite(a,b,zero(T),zero(T)) #No endpoint singularities
-Finite() = Finite(-1.0,1.0) #Canonical Domain
+Finite{T<:Number}(a::T,b::T,α::T,β::T,γ::T,δ::T) = Finite{T}(t->(a+b)/2+(b-a)/2*tanh(t),t->atanh((2t-a-b)/(b-a)),t->(b-a)/2*sech(t).^2,[a,b],[α,β],[γ,δ]) # Algebraic and logarithmic endpoint singularities
+Finite{T<:Number}(a::T,b::T,α::T,β::T) = Finite(a,b,α,β,zero(T),zero(T)) # Only algebraic endpoint singularities
+Finite{T<:Number}(a::T,b::T) = Finite(a,b,zero(T),zero(T)) # No endpoint singularities
+Finite{T<:Number}(::Type{T}) = Finite(-one(T),one(T)) # Canonical domain
+Finite() = Finite(Float64) # Canonical precision
 
 for dom in (:(Infinite1),:(Infinite2),:(SemiInfinite1),:(SemiInfinite2))
     @eval begin
@@ -34,3 +35,12 @@ Infinite1{T}(::Type{T}) = Infinite1{T}(sinh,asinh,cosh)
 Infinite2{T}(::Type{T}) = Infinite2{T}(identity,identity,t->0t+1)
 SemiInfinite1{T}(::Type{T}) = SemiInfinite1{T}(t->log(exp(t)+1),t->log(exp(t)-1),t->1./(1+exp(-t)))
 SemiInfinite2{T}(::Type{T}) = SemiInfinite2{T}(exp,log,exp)
+
+# singularities is a function that should have an override for every domain that has endpoint singularities.
+
+singularities{T<:Number}(domain::Domain{T},z::T) = one(T)
+function singularities{T<:Number}(domain::Finite{T},z::T)
+    α,β = domain.algebraic
+    γ,δ = domain.logarithmic
+    (2/(exp(2z)+1))^α*(2/(exp(-2z)+1))^β*log(2/(exp(2z)+1))^γ*log(2/(exp(-2z)+1))^δ
+end
--- src/SincFun.jl
+++ src/SincFun.jl
@@ -17,11 +17,12 @@ type sincfun{D,T}
     domain::D
 end
 
+ω{T<:Number}(t::T) = exp(-convert(T,π)/2*cosh(t))
+@vectorize_1arg Number ω
 
 function sincfun{T<:Number}(f::Function,domain::Domain{T})
     Tπ = convert(T,π)
     h(n) = log(Tπ*Tπ/2*n/(Tπ/2))/n
-    ω(t) = exp(-Tπ/2*cosh(t))
 
     n=2^3
     jh = h(n)*[-n:n]
@@ -35,7 +36,7 @@ function sincfun{T<:Number}(f::Function,domain::Domain{T})
     ϕpv = ϕpv[cutoff]
     ωscale = maxabs(fϕv.*ϕpv)
     ωv = ωscale*ωv[cutoff]
-    intold,intnew = Inf,h(n)*dot(fϕv,ϕpv)
+    intold,intnew = Inf,h(n)*dot(fϕv.^2,ϕpv)
     while n < 2^14 && abs(intold-intnew) > -3log(eps(T))*abs(intnew)*eps(T)
         n *= 2
         jh = h(n)*[-n:n]
@@ -49,7 +50,7 @@ function sincfun{T<:Number}(f::Function,domain::Domain{T})
         ϕpv = ϕpv[cutoff]
         ωscale = maxabs(fϕv.*ϕpv)
         ωv = ωscale*ωv[cutoff]
-        intold,intnew = intnew,h(n)*dot(fϕv,ϕpv)
+        intold,intnew = intnew,h(n)*dot(fϕv.^2,ϕpv)
     end
     j=[-n:n]
     n=length(cutoff[cutoff.==true])
@@ -61,7 +62,6 @@ sincfun(f::Function) = sincfun(f,Finite())
 function barycentric{D<:Domain,T<:Number}(sf::sincfun{D,T},x::T)
     Tπ = convert(T,π)
     t = asinh(2/Tπ*sf.domain.ψinv(x))
-    ω(t) = exp(-Tπ/2*cosh(t))
 
     if t < sf.j[1]*sf.h
         return sf.fϕv[1]
@@ -89,25 +89,19 @@ function barycentric{D<:Domain,T<:Number}(sf::sincfun{D,T},x::T)
     end
 end
 
-Base.getindex{D<:Domain,T<:Number,T1<:Number}(sf::sincfun{D,T},x::T1) = barycentric(sf,convert(promote_type(T,T1),x))
-function Base.getindex{D<:Finite,T<:Number,T1<:Number}(sf::sincfun{D,T},x::T1)
-    z = sf.domain.ψinv(x)
-    α,β = sf.domain.algebraic
-    γ,δ = sf.domain.logarithmic
-    barycentric(sf,convert(promote_type(T,T1),x))*(2/(exp(2z)+1))^α*(2/(exp(-2z)+1))^β*log(2/(exp(2z)+1))^γ*log(2/(exp(-2z)+1))^δ
+function Base.getindex{D<:Domain,T<:Number,T1<:Number}(sf::sincfun{D,T},x::T1)
+    xc = convert(promote_type(T,T1),x)
+    z = sf.domain.ψinv(xc)
+    barycentric(sf,xc)*singularities(sf.domain,z)
 end
 Base.getindex{D<:Domain,T<:Number,T1<:Number}(sf::sincfun{D,T},x::Vector{T1}) = T[sf[x[i]] for i=1:length(x)]
 
-Base.length(sf::sincfun) = sf.n
-
-Base.sum(sf::sincfun) = sf.h*dot(sf.fϕv,sf.ϕpv)
-function Base.sum{D<:Finite,T<:Number}(sf::sincfun{D,T})
-    Tπ = convert(T,π)
-    sinhv = Tπ/2*sinh(sf.j*sf.h)
-    α,β = sf.domain.algebraic
-    γ,δ = sf.domain.logarithmic
-    sf.h*dot(sf.fϕv,sf.ϕpv.*(2./(exp(2sinhv).+1)).^α.*(2./(exp(-2sinhv).+1)).^β.*log(2./(exp(2sinhv).+1)).^γ.*log(2./(exp(-2sinhv).+1)).^δ)
+function Base.sum{D<:Domain,T<:Number}(sf::sincfun{D,T})
+    sinhv = convert(T,π)/2*sinh(sf.j*sf.h)
+    sf.h*dot(sf.fϕv,sf.ϕpv.*T[singularities(sf.domain,sinhv[j]) for j=1:length(sinhv)])
 end
+
+Base.length(sf::sincfun) = sf.n
 
 include("fftBigFloat.jl")
 include("KrylovMethods.jl")
